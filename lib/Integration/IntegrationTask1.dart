ee42055609a96635a6df94109d58ef06e1305a7a
deepak <deepakpitta2001@gmail.com>
2025-03-17 11:16:31 +0530

    new changes

--- lib/Integration/IntegrationTask1.dart
+++ lib/Integration/IntegrationTask1.dart
@@ -11,39 +11,49 @@
 //     4. Use **loops** to filter and display **only the pending tasks**.
 
 
-class Task
-{
-  List<Map<String,dynamic> >objects =[{
-    "title": "deepak",
-    "description" : "this is lists",
-    "isCompleted":true
-  },
-    {
-      "title": "raj",
-      "description" : "this is new lists",
-      "isCompleted":false
-    },
-    {
-      "title": "lucky",
-      "description" : "this is old list",
-      "isCompleted":true
-    },];
 
-  List<dynamic> m=[];
-  List<dynamic> k=[];
-  List<dynamic> p=[];
-  void toggleTaskCompletion()
-  {
-    m=objects.where((ele)=>ele["isCompleted"]==true).map((ele)=>ele["${"title"},${"description"} Task Completed"]).toList();
-    print(m);
 
-    k=objects.where((ele)=>ele["isCompleted"]==false).map((ele)=>ele["${"title"},${"description"} Task was in Pending"]).toList();
-    for(int i=0;i<k.length;i++)
-      {
-        p.add("${k[i]}");
-      }
-    print(p);
+
+class Task {
+  String title;
+  String description;
+  bool isCompleted;
+  Task(this.title, this.description, this.isCompleted);
+
+}
+
+void main() {
+  toggleTaskCompletion();
+}
+
+void toggleTaskCompletion() {
+  List<Map<String, dynamic>> objects = [
+    {"title": "deepak", "description": "this is lists", "isCompleted": true},
+    {"title": "raj", "description": "this is new lists", "isCompleted": false},
+    {"title": "lucky", "description": "this is old list", "isCompleted": true},
+  ];
+  List<dynamic> m = [];
+  print("one");
+  m =
+      objects
+          .where((ele) => ele["isCompleted"] == true)
+          .map((ele) => ele["${"title"},${"description"} Task Completed"])
+          .toList();
+  print("${m.toList}");
+  print("two");
+  List<dynamic> k = [];
+  k =
+      objects
+          .where((ele) => ele["isCompleted"] == false)
+          .map((ele) => ele["${"title"},${"description"} Task was in Pending"])
+          .toList();
+  print("three");
+  List<dynamic> p = [];
+  for (int i = 0; i < k.length; i++) {
+    print("four");
+    p.add("${k[i]}");
+    print("five");
   }
-
-
+  print("six");
+  print(p);
 }
